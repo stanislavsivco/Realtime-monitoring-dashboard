13ee589fb56e2c98e61a709caa10f72254fcd63e
Peter Tomko <peter.tomko@kosickaakademia.sk>
2026-09-11 12:58:51 +0200

    feat/metric: Added MetricService and scheduled task for auto-generating metrics

--- backend/src/main/java/com/realtime_monitoring_dashboard/backend/BackendApplication.java
+++ backend/src/main/java/com/realtime_monitoring_dashboard/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.realtime_monitoring_dashboard.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling 
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/realtime_monitoring_dashboard/backend/service/MetricService.java
+++ backend/src/main/java/com/realtime_monitoring_dashboard/backend/service/MetricService.java
@@ -1,13 +1,16 @@
 package com.realtime_monitoring_dashboard.backend.service;
 
-import com.realtime_monitoring_dashboard.backend.model.Metric;
-import com.realtime_monitoring_dashboard.backend.repository.MetricRepository;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.time.LocalDateTime;
-import java.util.Random;
+import com.realtime_monitoring_dashboard.backend.model.Metric;
+import com.realtime_monitoring_dashboard.backend.repository.MetricRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service 
 @RequiredArgsConstructor 
@@ -16,6 +19,35 @@ public class MetricService {
 
     private final MetricRepository metricRepository;
     private final Random random = new Random();
-    
-    
+
+    public List<Metric> getAllMetrics() {
+        return metricRepository.findAll();
+    }
+
+    public Metric saveMetric(Metric metric) {
+        if (metric.getTimestamp() == null) {
+            metric.setTimestamp(LocalDateTime.now());
+        }
+        return metricRepository.save(metric);
+    }
+
+    public Metric generateAndSaveSimulatedMetric() {
+        double randomDisk = 10.0 + (85.0 * random.nextDouble());
+        double roundedDisk = Math.round(randomDisk * 100.0) / 100.0;
+
+        Metric metric = new Metric();
+        metric.setDisk(roundedDisk);
+        metric.setTimestamp(LocalDateTime.now());
+
+
+        return metricRepository.save(metric);
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void autoGenerateMetrics() {
+        Metric metric = generateAndSaveSimulatedMetric();
+        System.out.println("Automaticky uložená metrika: " + metric.getDisk() + "% o" + metric.getTimestamp());
+
+    }
+
 }
