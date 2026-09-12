32d433dcad1bd2848ae28c5954c3ddcb384c25ec
Peter-Tomko <peter.tomko@kosickaakademia.sk>
2026-09-12 14:00:23 +0200

    feat: add metric service and controller with disk monitoring

--- backend/src/main/java/com/realtime_monitoring_dashboard/backend/service/MetricService.java
+++ backend/src/main/java/com/realtime_monitoring_dashboard/backend/service/MetricService.java
@@ -8,7 +8,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.realtime_monitoring_dashboard.backend.model.Metric;
+import com.realtime_monitoring_dashboard.backend.repository.DeviceRepository;
 import com.realtime_monitoring_dashboard.backend.repository.MetricRepository;
+import com.realtime_monitoring_dashboard.backend.model.Device;
+import com.realtime_monitoring_dashboard.backend.repository.DeviceRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class MetricService {
 
     private final MetricRepository metricRepository;
+    private final DeviceRepository deviceRepository;
     private final Random random = new Random();
 
     public List<Metric> getAllMetrics() {
@@ -32,10 +36,14 @@ public class MetricService {
     }
 
     public Metric generateAndSaveSimulatedMetric() {
+
+        Device device = deviceRepository.findAll().get(0);
+
         double randomDisk = 10.0 + (85.0 * random.nextDouble());
         double roundedDisk = Math.round(randomDisk * 100.0) / 100.0;
 
         Metric metric = new Metric();
+        metric.setDevice(device);
         metric.setDisk(roundedDisk);
         metric.setTimestamp(LocalDateTime.now());
 
@@ -49,5 +57,9 @@ public class MetricService {
         System.out.println("Automaticky uložená metrika: " + metric.getDisk() + "% o" + metric.getTimestamp());
 
     }
+
+    public List<Metric> getMetricsByDeviceId(Long deviceId) {
+    return metricRepository.findByDeviceId(deviceId);
+}
 
 }
